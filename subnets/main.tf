4f6c8714dfa33db32adcea815fdfc8ef9b0cada8
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-07-15 22:37:32 +0530

    terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -9,3 +9,18 @@ resource "aws_subnet" "main" {
   },
     var.tags)
 }
+
+resource "aws_route_table" "table" {
+  vpc_id = var.vpc_id
+
+  tags = merge({
+    Name = "${var.env}-${var.subnet_name}"
+  },
+    var.tags)
+}
+
+#resource "aws_route_table_association" "association" {
+#  count          = length(aws_subnet.main.*.id)
+#  subnet_id      = element(aws_subnet.main.*.id, count.index)
+#  route_table_id = aws_route_table.table.id
+#}
